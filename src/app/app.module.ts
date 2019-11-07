bc9dcb4bfebfdb311092db4bf1dac7be931345e9
Hamid Djennaoui <hamid.djn@live.fr>
2019-11-07 23:40:28 +0100

    Ye dats the first f*in commit, I'm commited b*tch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HeadersPageComponent } from './pages/headers-page/headers-page.component';
+import { ButtonsPageComponent } from './pages/buttons-page/buttons-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HeadersPageComponent,
+    ButtonsPageComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/headers-page/headers-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-headers-page',
+  templateUrl: './headers-page.component.html',
+  styleUrls: ['./headers-page.component.scss']
+})
+export class HeadersPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
